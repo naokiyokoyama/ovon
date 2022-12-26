336691080255c90ab8321853485db792c25bdbb4
Naoki Yokoyama <naoki.yokoyama7@gmail.com>
2022-12-26 14:05:42 -0500

    updated setup to use post-Hydra habitat-lab

--- setup.sh
+++ setup.sh
@@ -1,16 +1,17 @@
 # Record of how the environment was set up
 pip install -e .  # install ovon as a package
 
-git clone git@github.com:vincentpierre/habitat-lab.git
-cd habitat-lab
-git checkout 9fd904925cf3ad222f3c88b5a3a0b90f5505f017
-git cherry-pick 6adc3b78cac8b98bbfa94d61cfa9f18250593651
-
+# Create conda environment. Mamba is recommended for faster installation.
 conda_env_name=ovon
-mamba create -n $conda_env_name python=3.7 cmake=3.14.0 -y
-mamba install -n $conda_env_name \
-  habitat-sim withbullet headless pytorch cudatoolkit=11.3 \
+conda create -n $conda_env_name python=3.7 cmake=3.14.0 -y
+conda install -n $conda_env_name \
+  habitat-sim=0.2.3 withbullet headless pytorch cudatoolkit=11.3 \
   -c pytorch -c nvidia -c conda-forge -c aihabitat -y
-mamba activate $conda_env_name
+
+# Install habitat-lab
+git clone git@github.com:facebookresearch/habitat-lab.git
+cd habitat-lab
+git checkout v0.2.3
+conda activate $conda_env_name
 pip install -e habitat-lab
 pip install -e habitat-baselines
