f6427973e5af7b7844e4590dfc9751ae6f8daea9
Naoki Yokoyama <naoki.yokoyama7@gmail.com>
2023-01-10 11:15:23 -0500

    changing conda to mamba for speed

--- setup.sh
+++ setup.sh
@@ -1,17 +1,20 @@
 # Record of how the environment was set up
-pip install -e .  # install ovon as a package
-
 # Create conda environment. Mamba is recommended for faster installation.
 conda_env_name=ovon
-conda create -n $conda_env_name python=3.7 cmake=3.14.0 -y
-conda install -n $conda_env_name \
+mamba create -n $conda_env_name python=3.7 cmake=3.14.0 -y
+mamba install -n $conda_env_name \
   habitat-sim=0.2.3 withbullet headless pytorch cudatoolkit=11.3 \
   -c pytorch -c nvidia -c conda-forge -c aihabitat -y
 
+# Install this repo as a package
+mamba activate $conda_env_name
+pip install -e .
+
 # Install habitat-lab
-git clone git@github.com:facebookresearch/habitat-lab.git
+git clone --branch v0.2.3 git@github.com:facebookresearch/habitat-lab.git
 cd habitat-lab
-git checkout v0.2.3
-conda activate $conda_env_name
 pip install -e habitat-lab
 pip install -e habitat-baselines
+
+pip install ftfy regex tqdm
+pip install git+https://github.com/openai/CLIP.git
