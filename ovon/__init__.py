abf5d3c4938af2a5ea017316c4a59509be968ea3
Naoki Yokoyama <naoki.yokoyama7@gmail.com>
2023-02-06 11:38:22 -0500

    added collision penalty for pointnav

--- ovon/__init__.py
+++ ovon/__init__.py
@@ -1,2 +1,4 @@
-from ovon import config, models
+from ovon import config
+from ovon.measurements import collision_penalty, sum_reward
 from ovon.models import clip_policy
+from ovon.trainers import ppo_trainer_no_2d, ver_trainer_no_2d
