29ae275eb3f1acd4cc7771c8702b0b9fe63b5fc5
Ram81 <ramramrakhya81@gmail.com>
2023-06-20 18:27:18 -0400

    [Dataset] Add dataset sampling scripts

--- ovon/utils/sample_episodes.py
+++ ovon/utils/sample_episodes.py
@@ -2,10 +2,59 @@ import argparse
 import glob
 import os.path as osp
 import random
+from collections import defaultdict
 
-import tqdm
+from tqdm import tqdm
 
-from ovon.utils.utils import load_dataset, write_dataset
+from ovon.utils.utils import count_episodes, load_dataset, write_dataset
+
+
+def sample_val(input_path, output_path, max_episodes):
+    files = glob.glob(osp.join(input_path, "*.json.gz"))
+    num_gz_files = len(files)
+
+    count, categories = count_episodes(input_path)
+    print("Total episodes: {}".format(count))
+
+    max_epsiodes_per_category = max_episodes // len(categories)
+    print("Max episodes per category: {}".format(max_epsiodes_per_category))
+
+    episodes_per_category = defaultdict(list)
+    for f in tqdm(files):
+        dataset = load_dataset(f)
+        for episode in dataset["episodes"]:
+            episodes_per_category[episode["object_category"]].append(episode)
+
+    sampled_buffer = []
+    episodes_per_scene = defaultdict(list)
+    total_episodes = 0
+    for category, episodes in episodes_per_category.items():
+        random.shuffle(episodes)
+        episodes_per_category[category] = episodes[:max_epsiodes_per_category]
+        sampled_buffer.extend(episodes[max_epsiodes_per_category + 1:])
+        total_episodes += len(episodes_per_category[category])
+        for episode in episodes_per_category[category]:
+            episodes_per_scene[episode["scene_id"]].append(episode)
+    
+    if total_episodes != max_episodes:
+        missing_episodes = max_episodes - total_episodes
+        sampled_episodes = random.sample(sampled_buffer, missing_episodes)
+        for episode in sampled_episodes:
+            episodes_per_scene[episode["scene_id"]].append(episode)
+
+    num_added = 0
+    for idx, file in enumerate(tqdm(files)):
+        dataset = load_dataset(file)
+        scene_id = dataset["episodes"][0]["scene_id"]
+
+        dataset["episodes"] = episodes_per_scene[scene_id]
+        num_added += len(dataset["episodes"])
+
+        output_file = osp.join(output_path, osp.basename(file))
+        print(f"Copied {len(dataset['episodes'])} episodes to {output_file}!")
+        write_dataset(dataset, output_file)
+
+    print(f"Added {num_added} episodes in total!")
 
 
 def main(input_path, output_path, max_episodes):
@@ -46,5 +95,10 @@ if __name__ == "__main__":
         help="Path to episode dir containing content/",
     )
     parser.add_argument("--max-episodes", type=int)
+    parser.add_argument("--val", dest="is_val", action="store_true")
     args = parser.parse_args()
-    main(args.input_path, args.output_path, args.max_episodes)
+
+    if args.is_val:
+        sample_val(args.input_path, args.output_path, args.max_episodes)
+    else:
+        main(args.input_path, args.output_path, args.max_episodes)
---
+++ ovon/utils/shuffle_episodes.py
@@ -0,0 +1,83 @@
+import argparse
+import copy
+import glob
+import os
+from collections import defaultdict
+
+from ovon.utils.utils import load_dataset, load_json, write_dataset
+
+
+def shuffle_episodes(dataset_path: str, output_path: str, meta_path: str = "data/hm3d_meta/val_splits.json"):
+    category_per_splits = load_json(meta_path)
+    splits = list(category_per_splits.keys())
+    print(splits)
+
+    scenes = glob.glob(os.path.join(dataset_path, splits[0], "content/*json.gz"))
+
+    # make directories
+    for split in splits:
+        os.makedirs(os.path.join(output_path, split, "content"), exist_ok=True)
+
+    for scene in scenes:
+        scene_id = scene.split("/")[-1]
+        scene_name = scene_id.split(".")[0]
+
+        goals_by_category = {}
+        episodes_by_category = defaultdict(list)
+        for split in splits:
+            path = os.path.join(dataset_path, split, "content", scene_id)
+            dataset = load_dataset(path)
+            
+            for goal_key, goal in dataset["goals_by_category"].items():
+                goals_by_category[goal_key] = goal
+            
+            for episode in dataset["episodes"]:
+                episodes_by_category[episode["object_category"]].append(episode)
+
+        for split in splits:
+            path = os.path.join(dataset_path, split, "content", scene_id)
+            dataset = load_dataset(path)
+
+            goals_before = len(dataset["goals_by_category"].keys())
+            episodes_before = len(dataset["episodes"])
+
+            dataset["goals_by_category"] = {}
+            dataset["episodes"] = []
+
+            for key in category_per_splits[split]:
+                g_key = "{}.basis.glb_{}".format(scene_name, key)
+                if goals_by_category.get(g_key) is None:
+                    continue
+                dataset["goals_by_category"][g_key] = goals_by_category[g_key]
+                dataset["episodes"].extend(episodes_by_category[key])
+            print("Split: {}, # of goals: {}/{}, # of episodes: {}/{}".format(split, len(dataset["goals_by_category"].keys()), goals_before, len(dataset["episodes"]), episodes_before))
+
+            op = os.path.join(output_path, split, "content", scene_id)
+            print("Output: {}".format(op))
+            write_dataset(dataset, op)
+
+    print("\n")
+    for split in splits:
+        files = glob.glob(os.path.join(output_path, split, "content/*json.gz"))
+
+        goals = []
+        episodes = 0
+        for f in files:
+            dataset = load_dataset(f)
+            episodes += len(dataset["episodes"])
+
+            goal_keys = [k.split("_")[-1] for k in dataset["goals_by_category"].keys()]
+            goals.extend(goal_keys)
+
+        diff = set(category_per_splits[split]).difference(set(goals))
+        print("Validating Split: {}, # of goals: {}, # of episodes: {}, Difference: {}".format(split, len(set(goals)), episodes, len(diff)))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset_path", type=str, default="data/hm3d_meta/val_splits.json")
+    parser.add_argument("--output_path", type=str, default="data/hm3d_meta/val_splits.json")
+
+    args = parser.parse_args()
+
+    shuffle_episodes(args.dataset_path, args.output_path)
