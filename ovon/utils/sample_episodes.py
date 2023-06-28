d2f19ca8308aa87262f413bbd4e5443144a33959
Ram81 <ramramrakhya81@gmail.com>
2023-06-28 11:48:22 -0400

    [Dataset] Add custom sampling

--- ovon/utils/sample_episodes.py
+++ ovon/utils/sample_episodes.py
@@ -1,12 +1,14 @@
 import argparse
 import glob
+import os
 import os.path as osp
 import random
 from collections import defaultdict
 
 from tqdm import tqdm
 
-from ovon.utils.utils import count_episodes, load_dataset, write_dataset
+from ovon.utils.utils import (count_episodes, load_dataset, load_json,
+                              write_dataset)
 
 
 def sample_val(input_path, output_path, max_episodes):
@@ -57,18 +59,57 @@ def sample_val(input_path, output_path, max_episodes):
     print(f"Added {num_added} episodes in total!")
 
 
+def sample_custom(input_path, output_path, episode_meta_file):
+    files = glob.glob(osp.join(input_path, "*.json.gz"))
+
+    episode_meta = load_json(episode_meta_file)
+
+    os.makedirs(output_path, exist_ok=True)
+
+    num_added = 0
+    eps_per_scene = []
+    for idx, file in enumerate(tqdm(files)):
+        dataset = load_dataset(file)
+        scene_id = file.split("/")[-1].split(".")[0]
+        print(file, scene_id)
+
+        episodes_by_category = defaultdict(list)
+        for episode in dataset["episodes"]:
+            episodes_by_category[episode["object_category"]].append(episode)
+        
+        dataset["episodes"] = []
+        for category in episode_meta[scene_id]:
+            min_episodes = min(episode_meta[scene_id][category], len(episodes_by_category[category]))
+            sampled_episodes = random.sample(episodes_by_category[category], min_episodes)
+            if min_episodes < episode_meta[scene_id][category]:
+                print(f"Warning: not enough episodes for {scene_id} {category}!")
+            dataset["episodes"].extend(sampled_episodes)
+
+        output_file = osp.join(output_path, osp.basename(file))
+        print(f"Copied {len(dataset['episodes'])} episodes to {output_file}!")
+        write_dataset(dataset, output_file)
+        num_added += len(dataset["episodes"])
+
+    print(f"Added {num_added} episodes in total!")
+    print(f"Episodes per scene: {eps_per_scene}")
+
+
 def main(input_path, output_path, max_episodes):
     files = glob.glob(osp.join(input_path, "*.json.gz"))
     num_gz_files = len(files)
 
+    os.makedirs(output_path, exist_ok=True)
+
     num_added = 0
-    for idx, file in enumerate(tqdm.tqdm(files)):
+    eps_per_scene = []
+    for idx, file in enumerate(tqdm(files)):
         dataset = load_dataset(file)
         random.shuffle(dataset["episodes"])
 
         num_left = max_episodes - num_added
         num_gz_remaining = num_gz_files - idx
         num_needed = min(num_left / num_gz_remaining, len(dataset["episodes"]))
+        eps_per_scene.append(num_needed)
 
         sampled_episodes = random.sample(dataset["episodes"], int(num_needed))
         num_added += len(sampled_episodes)
@@ -80,6 +121,7 @@ def main(input_path, output_path, max_episodes):
         write_dataset(dataset, output_file)
 
     print(f"Added {num_added} episodes in total!")
+    print(f"Episodes per scene: {eps_per_scene}")
 
 
 if __name__ == "__main__":
@@ -94,11 +136,18 @@ if __name__ == "__main__":
         type=str,
         help="Path to episode dir containing content/",
     )
+    parser.add_argument(
+        "--episode-meta-file",
+        type=str,
+        help="Path to num episode per category per scene meta file",
+    )
     parser.add_argument("--max-episodes", type=int)
     parser.add_argument("--val", dest="is_val", action="store_true")
     args = parser.parse_args()
 
-    if args.is_val:
+    if args.episode_meta_file is not None:
+        sample_custom(args.input_path, args.output_path, args.episode_meta_file)
+    elif args.is_val:
         sample_val(args.input_path, args.output_path, args.max_episodes)
     else:
         main(args.input_path, args.output_path, args.max_episodes)
